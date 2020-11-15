65ee18d6676a2d4c5ef660c17e6862591c3be16f
Sean <seanom96@gmail.com>
2020-11-15 09:23:26 +0000

    updated

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-const gulp = require("gulp");
-const del = require("del");
-gulp.task("clean", () => {
-  return del(["docs/*.js", "docs/*.css", "docs/*.html", "docs/images/**"]);
-});
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,28 +3,36 @@ let container = document.getElementsByClassName("container")[0];
 console.log(container);
 let message = [
   "Hello there!",
-  "My name is Sean.",
+  "My name is Seán.",
   "Looks like you found my github page.",
   "Feel free to browse my profile",
-  "Here",
+  "<a href='https://github.com/seanomeara96'>Here</a>",
   "And see what kind of stuff I've been working on.",
 ];
 let count = 0;
 let letterIndex;
 let displayText;
-function readNextSentence() {
+function readSentence() {
   letterIndex = 0;
   displayText = [];
   if (count < message.length) {
-    readLetters();
+    if (count != 4) {
+      readLetters();
+    } else {
+      container.innerHTML = message[count];
+      setTimeout(() => {
+        nextSentence();
+      }, 700);
+    }
   } else {
     count = 0;
+    readLetters();
   }
 }
-function readLetters(cb) {
+function readLetters() {
   if (letterIndex <= message[count].length) {
     //do something
-    container.innerText = message[count].substring(0, letterIndex);
+    container.innerHTML = message[count].substring(0, letterIndex);
     letterIndex++;
     setTimeout(() => {
       readLetters();
@@ -36,10 +44,10 @@ function readLetters(cb) {
 function nextSentence() {
   count++;
   setTimeout(() => {
-    readNextSentence();
+    readSentence();
   }, 700);
 }
-readNextSentence();
+readSentence();
 /**
  * an array of sentences
  * for each sentence
